0f79396644cac19587dde1380c3f3a04b6436d7c
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2022-10-28 16:57:37 +0300

    add code conventions;

--- crates/wasm/src/lib.rs
+++ crates/wasm/src/lib.rs
@@ -1,13 +1,25 @@
 //! Performs translation from a wasm module in binary format to the in-memory form
 //! of c2zk IR.
-//! The main function of this module is [`translate_module`](fn.translate_module.html).
 
-#![deny(
-    missing_docs,
-    trivial_numeric_casts,
-    unused_extern_crates,
-    unstable_features
-)]
+// Coding conventions
+#![deny(unsafe_code)]
+#![deny(non_camel_case_types)]
+#![deny(non_snake_case)]
+#![deny(unused_mut)]
+#![deny(dead_code)]
+#![deny(unused_imports)]
+#![deny(missing_docs)]
+#![deny(trivial_numeric_casts)]
+#![deny(unused_extern_crates)]
+#![deny(unstable_features)]
+// Clippy exclusions
+#![deny(rustdoc::broken_intra_doc_links)]
+#![deny(clippy::wildcard_enum_match_arm)]
+#![deny(clippy::unwrap_used)]
+#![deny(clippy::expect_used)]
+// #![deny(clippy::todo)]
+#![deny(clippy::unimplemented)]
+#![deny(clippy::panic)]
 #![warn(unused_import_braces)]
 
 mod code_translator;
