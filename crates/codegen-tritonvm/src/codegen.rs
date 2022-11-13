effae5e976fcf655c49bc16af3d000723d94dfd2
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2022-11-13 18:06:30 +0200

    draft Inst::I32Const emit;

--- crates/codegen-tritonvm/src/codegen.rs
+++ crates/codegen-tritonvm/src/codegen.rs
@@ -1,16 +1,28 @@
 use c2zk_codegen_shared::CodegenError;
 use c2zk_ir::ir::Inst;
+use triton_vm::instruction::Instruction;
+use twenty_first::shared_math::b_field_element::BFieldElement;
 
 use crate::TritonTargetConfig;
 
 #[allow(unused_variables)]
 pub fn emit(ins: &Inst, config: &TritonTargetConfig) -> Result<Vec<u8>, CodegenError> {
+    let mut code = Vec::new();
     match ins {
         Inst::Unreachable => todo!(),
         Inst::Nop => todo!(),
         Inst::End => todo!(),
         Inst::Return => todo!(),
-        Inst::I32Const { value } => todo!(),
+        Inst::I32Const { value } => code.push(Instruction::Push(felt(*value))),
     }
-    Ok(vec![])
+    Ok(serialize(code))
+}
+
+fn felt(v: i32) -> BFieldElement {
+    // TODO: implement according to https://github.com/Neptune-Crypto/twenty-first/issues/32
+    BFieldElement::new(v as u64)
+}
+
+fn serialize(_code: Vec<Instruction>) -> Vec<u8> {
+    todo!()
 }
