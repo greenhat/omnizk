71f479a75d1b3c6abccc65fe211ad57afff26f37
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2023-07-01 07:41:10 +0300

    introduce emit_masm_* macros;

--- crates/codegen-midenvm/src/codegen/emit/emit_instr.rs
+++ crates/codegen-midenvm/src/codegen/emit/emit_instr.rs
@@ -12,33 +12,56 @@ pub trait EmitMasm: Op {
     fn emit_masm(&self, ctx: &Context, builder: &mut MidenAssemblyBuilder);
 }
 
-#[cast_to]
-impl EmitMasm for ConstantOp {
-    fn emit_masm(&self, ctx: &Context, builder: &mut MidenAssemblyBuilder) {
-        let val = self.get_value(ctx);
-        builder.push(val.into());
-    }
+macro_rules! emit_masm {
+    ($op:ty, $builder_method:ident) => {
+        #[cast_to]
+        impl EmitMasm for $op {
+            fn emit_masm(&self, ctx: &Context, builder: &mut MidenAssemblyBuilder) {
+                builder.$builder_method();
+            }
+        }
+    };
 }
 
-#[cast_to]
-impl EmitMasm for AddOp {
-    fn emit_masm(&self, ctx: &Context, builder: &mut MidenAssemblyBuilder) {
-        builder.add();
-    }
+macro_rules! emit_masm_param {
+    ($op:ty, $builder_method:ident, $builder_method_param:ident) => {
+        #[cast_to]
+        impl EmitMasm for $op {
+            fn emit_masm(&self, ctx: &Context, builder: &mut MidenAssemblyBuilder) {
+                builder.$builder_method(self.$builder_method_param(ctx).into());
+            }
+        }
+    };
 }
 
-#[cast_to]
-impl EmitMasm for ExecOp {
-    fn emit_masm(&self, ctx: &Context, builder: &mut MidenAssemblyBuilder) {
-        let callee = self.get_callee_sym(ctx);
-        builder.exec(callee);
-    }
-}
+emit_masm_param!(ConstantOp, push, get_value);
 
-#[cast_to]
-impl EmitMasm for LocLoadOp {
-    fn emit_masm(&self, ctx: &Context, builder: &mut MidenAssemblyBuilder) {
-        let index = self.get_index_as_u32(ctx);
-        builder.loc_load(index);
-    }
-}
+// #[cast_to]
+// impl EmitMasm for ConstantOp {
+//     fn emit_masm(&self, ctx: &Context, builder: &mut MidenAssemblyBuilder) {
+//         let val = self.get_value(ctx);
+//         builder.push(val.into());
+//     }
+// }
+
+emit_masm!(AddOp, add);
+
+emit_masm_param!(ExecOp, exec, get_callee_sym);
+
+// #[cast_to]
+// impl EmitMasm for ExecOp {
+//     fn emit_masm(&self, ctx: &Context, builder: &mut MidenAssemblyBuilder) {
+//         let callee = self.get_callee_sym(ctx);
+//         builder.exec(callee);
+//     }
+// }
+
+emit_masm_param!(LocLoadOp, loc_load, get_index_as_u32);
+
+// #[cast_to]
+// impl EmitMasm for LocLoadOp {
+//     fn emit_masm(&self, ctx: &Context, builder: &mut MidenAssemblyBuilder) {
+//         let index = self.get_index_as_u32(ctx);
+//         builder.loc_load(index);
+//     }
+// }
